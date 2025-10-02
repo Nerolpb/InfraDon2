71a38e4730af820c65d8ddeb047e08f12b193965
Nerolpb <christophe.25.03@hotmail.com>
2025-10-02 12:47:15 +0200

    Bunch of things to test

---
+++ InfraDon2 - noSQL/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+//DATA VARIABLES
+const counter = ref(0);
+
+//FUNCTIONS / METHODS -> CONTROLLER
+const increment = () => {
+  counter.value++
+}
+
+</script>
+
+<template>
+  <h1>Welcome</h1>
+  <p>Counter: {{ counter }}</p>
+  <button @click="increment">Increment</button>
+</template>
